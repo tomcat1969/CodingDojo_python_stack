3b3d7702e9acbf9b9754170d7a3e1af0e53935c8
huanglin <tomcat1969@gmail.com>
2020-01-06 12:07:44 -0800

    function intermediate2

---
+++ python/fundamentals/functionInter2.py
@@ -0,0 +1,95 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+
+x[1][0] = 15
+print(x)
+students[0]['last_name'] = 'Bryant'
+print(students)
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+z[0]['y'] =30
+print(z)
+######################################
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+
+def iterateDictionary(list):
+	for i in range(len(list)):
+		print(f"first_name - {students[i]['first_name']}, last_name - {students[i]['last_name']}")
+
+iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+def iterateDictionary2(key_name, some_list):
+	for i in range(len(some_list)):
+		print(i,some_list[i][key_name])
+
+
+iterateDictionary2("last_name", students)
+
+print("*"*30)##########################
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+
+
+
+
+def printInfo(dic):
+	for key in dic.keys():
+		print(f"{len(dic[key])} LOCATIONS")
+		for item in dic[key]:
+			print(item)
+		print()	
+		
+printInfo(dojo)			
+
+
+
+
+
+# printInfo(dojo)
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+
+
+
