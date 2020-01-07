07a673bcee9b36352f8c7b5f3fdbd01c6967a568
huanglin <tomcat1969@gmail.com>
2020-01-06 17:04:13 -0800

    fixed bugs

--- python/fundamentals/userclass.py
+++ python/fundamentals/userclass.py
@@ -7,19 +7,24 @@ class User:
 	    self.account_balance = self.account_balance - amount
 
 	def display_user_balance(self):
-		print(self.account_balance)
+	    print(self.account_balance)
 
 	def transfer_money(self,other_user,amount):
 		self.account_balance = self.account_balance - amount
-		other_user.account_balance = other_user.account_balance + amount	    
+		other_user.account_balance = other_user.account_balance + amount
+
+	def make_deposit(self, amount):
+		self.account_balance = self.account_balance + amount
+		return self
 
 
 user1 = User("huanglin","tomcat1969@gmail.com")
-user1.account_balance = 100
+# user1.account_balance = 100
 user2 = User("Marry","Marry@163.com")
-user2.account_balance = 100
-print(f"{user1.name} has {user1.account_balance}")
-print(f"{user2.name} has {user2.account_balance}")
-user1.transfer_money(user2,20)
-print(f"{user1.name} has {user1.account_balance}")
-print(f"{user2.name} has {user2.account_balance}")
+# user2.account_balance = 100
+# print(f"{user1.name} has {user1.account_balance}")
+# print(f"{user2.name} has {user2.account_balance}")
+# user1.transfer_money(user2,20)
+# print(f"{user1.name} has {user1.account_balance}")
+# print(f"{user2.name} has {user2.account_balance}")
+user1.make_deposit(100).make_deposit(190).display_user_balance()
