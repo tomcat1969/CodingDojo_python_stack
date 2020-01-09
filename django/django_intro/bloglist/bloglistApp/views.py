35d13f76b9b7b4b4e1d526ac745952c929b6c29a
huanglin <tomcat1969@gmail.com>
2020-01-08 17:04:49 -0800

    make static

--- django/django_intro/bloglist/bloglistApp/views.py
+++ django/django_intro/bloglist/bloglistApp/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render,HttpResponse,redirect
 def index(request):
-    return HttpResponse("<h1>Placeholder to display a list of all blogs<h1>/")
+    #return HttpResponse("<h1>Placeholder to display a list of all blogs<h1>/")
+    context = {"name": "Noelle",
+                "favorite_color": "turquoise",
+                "pets": ["Bruce", "Fitz", "Georgie"]
+               }
+    return render(request, "index.html", context)
+
 def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
 def create(request):
     return redirect("/")
 def show(request,number):
-    html = "Placeholder to display blog number:  {%s}" %number
-    return HttpResponse(html)
+    #html = f"Placeholder to display blog number:  {number}"
+    return HttpResponse(f"Placeholder to display blog number:  {number}")
 def edit(request,number):
     return HttpResponse("placeholder to edit blog {%s}" %number)
 def delete(request,number):
