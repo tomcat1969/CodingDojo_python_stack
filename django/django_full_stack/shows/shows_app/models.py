904f792dd37c4340e641d1ae88f61d3cb182541c
huanglin <tomcat1969@gmail.com>
2020-01-21 16:14:32 -0800

    finish midWay

--- django/django_full_stack/shows/shows_app/models.py
+++ django/django_full_stack/shows/shows_app/models.py
@@ -1,10 +1,57 @@
 from django.db import models
+from datetime import date
+from datetime import datetime
+
+
+def isPast(list_date):
+    year = int(list_date[0])
+    month = int(list_date[1])
+    day = int(list_date[2])
+    today = date.today()
+    if year > today.year:
+        return False
+    elif year < today.year:
+        return True
+    else:
+        if month > today.month:
+            return False
+        elif month < today.month:
+            return True
+        else:
+            if day > today.day:
+                return False
+            else:
+                return True
+
+
+class ShowManager(models.Manager):
+    def basic_validator(self,post_data):
+        errors = {}
+        if len(post_data['title']) < 2:
+            errors['title'] = "title should be at least 2 characters"
+        if len(post_data['network']) < 3:
+            errors['network'] = "network should be at least 3 characters"
+        if len(post_data['desc']) < 10 and len(post_data['desc']) != 0:
+            errors['desc'] = "Description should be at least 10 characters or 0"
+        mydate = post_data['release_date']
+        list_date = mydate.split("-")
+        if isPast(list_date) != True:
+            errors['release_date'] = "release date should be in the past"
+        return errors
+
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
-    release_date = models.CharField(max_length=255)
+    release_date = models.DateField()
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
 
+
+# print("hello")
+# str = '2024-09-07'
+# l = str.split("-")
+# print(l)
+# print(isPast(l))
--- django/django_full_stack/shows/shows_app/views.py
+++ django/django_full_stack/shows/shows_app/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render,redirect
 from .models import Show
+from django.contrib import messages
 def AllShows(request):
+    print("code start here", "*" *40)
+    # str = "hello"
+    # str2 = "elloh"
+    # if str == str2:
+    #     print(True)
+    # else:
+    #     print(False)
+
+
+
+
+
+
+
+    print("code end here", "*" *40)
     context={
         "all_the_shows":Show.objects.all()
     }
     return render(request, "AllShows.html",context)
 def AddShow(request):
+    print("AddShow is running",request.POST)
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for k, v in errors.items():
+            messages.error(request, v)
+
+        return redirect("/shows/new")
+
+    ###
+
+
     title=request.POST['title']
     network=request.POST['network']
     release_date=request.POST['release_date']
@@ -17,8 +44,10 @@ def new(request):
 def editShow(request,show_id):
     myShow = Show.objects.get(id=show_id)
     context={
-        "myShow":myShow
+        "myShow":myShow,
+        "release_date":str(myShow.release_date)
     }
+    print(myShow.release_date)
     return render(request,"edit.html",context)
 def update(request):
     show_id=request.POST['show_id']
