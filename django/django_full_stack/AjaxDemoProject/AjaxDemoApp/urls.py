a1cb5a9816eebea73701cdd2b72b4fd6809bd492
huanglin <tomcat1969@gmail.com>
2020-01-29 11:45:09 -0800

    simple ajax

--- django/django_full_stack/AjaxDemoProject/AjaxDemoApp/urls.py
+++ django/django_full_stack/AjaxDemoProject/AjaxDemoApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('addPost',views.addPost)
+    path('addPost',views.addPost),
+    path('ajaxmessages', views.ajaxPost)
 
 ]
--- django/django_full_stack/AjaxDemoProject/AjaxDemoApp/views.py
+++ django/django_full_stack/AjaxDemoProject/AjaxDemoApp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render,redirect
 from .models import *
 
 def index(request):
+
     context = {
         "all_the_messages": Messages.objects.all()
+
     }
     return render(request,"index.html",context)
 
@@ -11,4 +13,10 @@ def index(request):
 def addPost(request):
     Messages.objects.create(message=request.POST['message'])
     print('add a message in date base')
-    return redirect('/')
+    return redirect('/ajaxmessages')
+
+def ajaxPost(request):
+    context = {
+        "all_the_messages": Messages.objects.all()
+    }
+    return render(request, "messages.html", context)
